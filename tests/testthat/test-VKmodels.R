93f0764fc05f51e3e80017f2f8fa554deaac4a6c
ojcharles <oscarjamescharles@gmail.com>
2022-09-07 12:12:38 +0100

    sars2 models

--- tests/testthat/test-VKmodels.R
+++ tests/testthat/test-VKmodels.R
@@ -6,7 +6,6 @@ test_that("model_VK_1", {
   fit = suppressWarnings(nlmixr2::nlmixr(object = eval(parse( text = paste(text = m1_MwTCLESIC(T,T,T,T,T)))),
                                 data2_cmv_untreated,
                                 list(print=0), est = "saem"))
-  
   expect_identical(length(fit$parFixedDf$Estimate) , as.integer(4))
 })
 
@@ -16,7 +15,6 @@ test_that("model_VK_2", {
   fit = suppressWarnings(nlmixr2::nlmixr(object = eval(parse( text = paste(text = m2_MwTCLEDIS(T,T,T,T,T)))),
                                 data2_cmv_untreated,
                                 list(print=0), est = "saem"))
-  
   expect_identical(length(fit$parFixedDf$Estimate) , as.integer(7))
 })
 
@@ -25,7 +23,6 @@ test_that("model_VK_3", {
   fit = suppressWarnings(nlmixr2::nlmixr(object = eval(parse( text = paste(text = m3_EDICwTCL(T,T,T)))),
                                 data2_cmv_untreated,
                                 list(print=0), est = "saem"))
-  
   expect_identical(length(fit$parFixedDf$Estimate) , as.integer(7))
 })
 
@@ -33,7 +30,6 @@ test_that("model_VK_4", {
   fit = suppressWarnings(nlmixr2::nlmixr(object = eval(parse( text = paste(text = m4_EDICwTCL(T,T)))),
                                 data2_cmv_untreated,
                                 list(print=0), est = "saem"))
-  
   expect_identical(length(fit$parFixedDf$Estimate) , as.integer(8))
 })
 
@@ -42,7 +38,6 @@ test_that("model_VK_5", {
   fit = suppressWarnings(nlmixr2::nlmixr(object = eval(parse( text = paste(text = m5_EDICwTCL(T,T)))),
                                 data2_cmv_untreated,
                                 list(print=0), est = "saem"))
-  
   expect_identical(length(fit$parFixedDf$Estimate) , as.integer(7))
 })
 
@@ -51,7 +46,6 @@ test_that("model_VK_6", {
   fit = suppressWarnings(nlmixr2::nlmixr(object = eval(parse( text = paste(text = m6_EDICwTCL(T,T,T,T)))),
                                 data2_cmv_untreated,
                                 list(print=0), est = "saem"))
-  
   expect_identical(length(fit$parFixedDf$Estimate) , as.integer(5))
 })
 
@@ -59,7 +53,6 @@ test_that("model_VK_7", {
   fit = suppressWarnings(nlmixr2::nlmixr(object = eval(parse( text = paste(text = m7_SMEIMCs(T,T)))),
                                 data2_cmv_untreated,
                                 list(print=0), est = "saem"))
-  
   expect_identical(length(fit$parFixedDf$Estimate) , as.integer(5))
 })
 
@@ -67,7 +60,6 @@ test_that("model_VK_8", {
   fit = suppressWarnings(nlmixr2::nlmixr(object = eval(parse( text = paste(text = m8_SMEIMCs(T)))),
                                 data2_cmv_untreated,
                                 list(print=0), est = "saem"))
-  
   expect_identical(length(fit$parFixedDf$Estimate) , as.integer(5))
 })
 
@@ -75,7 +67,56 @@ test_that("model_VK_9", {
   fit = suppressWarnings(nlmixr2::nlmixr(object = eval(parse( text = paste(text = m9_SMEIMCs(T)))),
                                 data2_cmv_untreated,
                                 list(print=0), est = "saem"))
-  
   expect_identical(length(fit$parFixedDf$Estimate) , as.integer(5))
 })
+
+test_that("model_VK_10", {
+  fit = suppressWarnings(nlmixr2::nlmixr(object = eval(parse( text = paste(text = m10_SARSTCLM(F)))),
+                                         data2_cmv_untreated,
+                                         list(print = 0), est = "saem"))
+  expect_identical(length(fit$parFixedDf$Estimate) , as.integer(8))
+})
+
+test_that("model_VK_11", {
+  fit = suppressWarnings(nlmixr2::nlmixr(object = eval(parse( text = paste(text = m11_SARSTCLM(F)))),
+                                         data2_cmv_untreated,
+                                         list(print=0), est = "saem"))
+  expect_identical(length(fit$parFixedDf$Estimate) , as.integer(6))
+})
+
+test_that("model_VK_12", {
+  fit = suppressWarnings(nlmixr2::nlmixr(object = eval(parse( text = paste(text = m12_SARSTCLMwEP(F)))),
+                                         data2_cmv_untreated,
+                                         list(print=0), est = "saem"))
+  expect_identical(length(fit$parFixedDf$Estimate) , as.integer(10))
+})
+
+test_that("model_VK_13", {
+  fit = suppressWarnings(nlmixr2::nlmixr(object = eval(parse( text = paste(text = m13_SARSTCLMwImmune(F)))),
+                                         data2_cmv_untreated,
+                                         list(print=0), est = "saem"))
+  expect_identical(length(fit$parFixedDf$Estimate) , as.integer(14))
+})
+
+test_that("model_VK_14", {
+  fit = suppressWarnings(nlmixr2::nlmixr(object = eval(parse( text = paste(text = m14_SARSTCLMwImmune(F)))),
+                                         data2_cmv_untreated,
+                                         list(print=0), est = "saem"))
+  expect_identical(length(fit$parFixedDf$Estimate) , as.integer(14))
+})
+
+test_that("model_VK_15", {
+  fit = suppressWarnings(nlmixr2::nlmixr(object = eval(parse( text = paste(text = m15_SARSTCLMwImmune(F)))),
+                                         data2_cmv_untreated,
+                                         list(print=0), est = "saem"))
+  expect_identical(length(fit$parFixedDf$Estimate) , as.integer(14))
+})
+
+test_that("model_VK_16", {
+  fit = suppressWarnings(nlmixr2::nlmixr(object = eval(parse( text = paste(text = m16_SARSTCLMwImmune(F)))),
+                                         data2_cmv_untreated,
+                                         list(print=0), est = "saem"))
+  expect_identical(length(fit$parFixedDf$Estimate) , as.integer(14))
+})
+
 
