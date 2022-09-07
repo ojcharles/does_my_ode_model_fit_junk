3be74e9f2defd0e00990c40ca0f842152b128a24
ojcharles <oscarjamescharles@gmail.com>
2022-09-07 12:23:06 +0100

    sars2 models (#5)

--- R/viral_kinetic_models.R
+++ R/viral_kinetic_models.R
@@ -534,3 +534,467 @@ m9_SMEIMCs = function(fix_E0 = T, ...){
   return(m)
 }
 
+
+
+##### SARS-CoV-2 Target Cell Limited Models
+
+#' m10_SARSTCLM
+#' @return VK nlmixr function string
+#' @export
+m10_SARSTCLM = function(fix_E0 = T, ...){
+  m = "function() {
+  ini({
+    tbeta = runif(1,-10,-2)
+    tro = runif(1,-6, -0)
+    tc = runif(1,-20, -2)
+    td = runif(1,-4,3)
+    tvirus = runif(1,-2, 2)
+    tinfected = runif(1,0, 6)
+    ttarget = runif(1,0, 6)
+
+    eta.ro ~ 2
+    eta.d ~ 2
+    eta.beta ~ 2
+    eta.virus ~ 1
+    eta.infected ~ 1
+    eta.target ~ 1
+    eta.c ~ 2
+
+    add.sd = 10
+  })
+  model({
+    beta = exp(tbeta + eta.beta)
+    ro = exp(tro + eta.ro)
+    c = exp(tc + eta.c)
+    d = exp(td + eta.d)
+    virus0 = exp(tvirus + eta.virus)
+    infected0 = exp(tinfected + eta.infected)
+    target0 = exp(ttarget + eta.target)
+    
+    target(0) = target0
+    infected(0) = infected0
+    virus(0) = virus0
+
+    d/dt(target) = - beta * virus * target
+    d/dt(virus) = ro * infected - c * virus
+    d/dt(infected) = beta * virus * target - d * infected
+
+    VL = log(virus)
+    VL ~ add(add.sd)
+  })
+}"
+  
+  # if(fix_E0){
+  #   m = gsub("tE0 = runif(1,-6,1)","",m,fixed = T)
+  #   m = gsub("eta.E0 ~ 2","",m,fixed = T)
+  #   m = gsub("E0 = exp(tE0 + eta.E0)","E0 = 0",m, fixed = T)
+  # }
+  return(m)
+}
+
+#' m11_SARSTCLM - simplified
+#' @return VK nlmixr function string
+#' @export
+m11_SARSTCLM = function(fix_E0 = T, ...){
+  m = "function() {
+  ini({
+    tbeta = runif(1,-6,1)
+    tgamma = runif(1,-12, -4)
+    td = runif(1,-6,1)
+    tvirus = runif(1,-2, 2)
+    tuninfected_frac = runif(1,-2, 2)
+
+    eta.beta ~ 2
+    eta.gamma ~ 2
+    eta.d ~ 2
+    eta.virus ~ 1
+    eta.uninfected_frac ~ 1
+
+    add.sd = 10
+  })
+  model({
+    beta = exp(tbeta + eta.beta)
+    gamma = exp(tgamma + eta.gamma)
+    d = exp(td + eta.d)
+    virus0 = exp(tvirus + eta.virus)
+    uninfected_frac0 = exp(tuninfected_frac + eta.uninfected_frac)
+    
+    uninfected_frac(0) = uninfected_frac0
+    virus(0) = virus0
+
+    d/dt(uninfected_frac) = - beta * uninfected_frac * virus
+    d/dt(virus) = gamma * uninfected_frac * virus - d * virus
+
+    VL = log(virus)
+    VL ~ add(add.sd)
+  })
+}"
+  
+  # if(fix_E0){
+  #   m = gsub("tE0 = runif(1,-6,1)","",m,fixed = T)
+  #   m = gsub("eta.E0 ~ 2","",m,fixed = T)
+  #   m = gsub("E0 = exp(tE0 + eta.E0)","E0 = 0",m, fixed = T)
+  # }
+  return(m)
+}
+
+
+
+##### SARS-CoV-2 Target Cell Limited Models with eclipse phase
+
+#' m12_SARSTCLMwEP
+#' @return VK nlmixr function string
+#' @export
+m12_SARSTCLMwEP = function(fix_E0 = T, ...){
+  m = "function() {
+  ini({
+    tbeta = runif(1,-10,-2)
+    tro = runif(1,-6, -0)
+    tc = runif(1,-20, -2)
+    td = runif(1,-4,3)
+    tk = runif(1,-4,3)
+    tvirus = runif(1,-2, 2)
+    tinfected_eclipse = runif(1,0, 6)
+    tinfected_lytic = runif(1,0, 6)
+    ttarget = runif(1,0, 6)
+
+    eta.ro ~ 2
+    eta.d ~ 2
+    eta.beta ~ 2
+    eta.k ~ 1
+    eta.virus ~ 1
+    eta.infected_eclipse ~ 1
+    eta.infected_lytic ~ 1
+    eta.target ~ 1
+    eta.c ~ 2
+
+    add.sd = 10
+  })
+  model({
+    beta = exp(tbeta + eta.beta)
+    ro = exp(tro + eta.ro)
+    c = exp(tc + eta.c)
+    d = exp(td + eta.d)
+    k = exp(tk + eta.k)
+    virus0 = exp(tvirus + eta.virus)
+    infected_eclipse0 = exp(tinfected_eclipse + eta.infected_eclipse)
+    infected_lytic0 = exp(tinfected_lytic + eta.infected_lytic)
+    target0 = exp(ttarget + eta.target)
+    
+    target(0) = target0
+    infected_eclipse(0) = infected_eclipse0
+    infected_lytic(0) = infected_lytic0
+    virus(0) = virus0
+
+    d/dt(target) = - beta * virus * target
+    d/dt(infected_eclipse) = beta * virus * target - k * infected_eclipse
+    d/dt(infected_lytic) = k * infected_eclipse - d * infected_lytic
+    d/dt(virus) = ro * infected_lytic - c * virus - beta * virus * target
+    
+    VL = log(virus)
+    VL ~ add(add.sd)
+  })
+}"
+  
+  # if(fix_E0){
+  #   m = gsub("tE0 = runif(1,-6,1)","",m,fixed = T)
+  #   m = gsub("eta.E0 ~ 2","",m,fixed = T)
+  #   m = gsub("E0 = exp(tE0 + eta.E0)","E0 = 0",m, fixed = T)
+  # }
+  return(m)
+}
+
+
+
+##### SARS-CoV-2 Target Cell Limited Models with Immune Effector
+
+#' m13_SARSTCLMwImmune   immune  recovers target cells
+#' @return VK nlmixr function string
+#' @export
+m13_SARSTCLMwImmune = function(fix_E0 = T, ...){
+  m = "function() {
+  ini({
+    tbeta = runif(1,-10,-2)
+    tro = runif(1,-6, -0)
+    tc = runif(1,-20, -2)
+    td = runif(1,-4,3)
+    tk = runif(1,-4,3)
+    tvphi = runif(1,-4,3)
+    tvtheta = runif(1,-4,3)
+    tdImm = runif(1,-4,1)
+    tvirus = runif(1,-2, 2)
+    tinfected_eclipse = runif(1,0, 6)
+    tinfected_lytic = runif(1,0, 6)
+    ttarget = runif(1,0, 6)
+    tImmuneEffector = runif(1,0,6)
+
+    eta.ro ~ 2
+    eta.d ~ 2
+    eta.beta ~ 2
+    eta.k ~ 1
+    eta.vphi ~ 1
+    eta.vtheta ~ 1
+    eta.dImm ~ 1
+    eta.virus ~ 1
+    eta.infected_eclipse ~ 1
+    eta.infected_lytic ~ 1
+    eta.target ~ 1
+    eta.c ~ 2
+    eta.ImmuneEffector ~ 1
+
+    add.sd = 10
+  })
+  model({
+    beta = exp(tbeta + eta.beta)
+    ro = exp(tro + eta.ro)
+    c = exp(tc + eta.c)
+    d = exp(td + eta.d)
+    k = exp(tk + eta.k)
+    vphi = exp(tvphi + eta.vphi)
+    vtheta = exp(tvtheta + eta.vtheta)
+    dImm = exp(tdImm + eta.dImm)
+    virus0 = exp(tvirus + eta.virus)
+    infected_eclipse0 = exp(tinfected_eclipse + eta.infected_eclipse)
+    infected_lytic0 = exp(tinfected_lytic + eta.infected_lytic)
+    target0 = exp(ttarget + eta.target)
+    ImmuneEffector0 = exp(tImmuneEffector + eta.ImmuneEffector)
+    
+    target(0) = target0
+    infected_eclipse(0) = infected_eclipse0
+    infected_lytic(0) = infected_lytic0
+    virus(0) = virus0
+    ImmuneEffector(0) = ImmuneEffector0
+
+    d/dt(target) = - beta * virus * target - vphi * (ImmuneEffector / (ImmuneEffector + vtheta) ) * target
+    d/dt(infected_eclipse) = beta * virus * target - k * infected_eclipse
+    d/dt(infected_lytic) = k * infected_eclipse - d * infected_lytic
+    d/dt(virus) = ro * infected_lytic - c * virus - beta * virus * target
+    d/dt(ImmuneEffector) = infected_lytic - dImm * ImmuneEffector
+    
+    VL = log(virus)
+    VL ~ add(add.sd)
+  })
+}"
+  
+  # if(fix_E0){
+  #   m = gsub("tE0 = runif(1,-6,1)","",m,fixed = T)
+  #   m = gsub("eta.E0 ~ 2","",m,fixed = T)
+  #   m = gsub("E0 = exp(tE0 + eta.E0)","E0 = 0",m, fixed = T)
+  # }
+  return(m)
+}
+
+#' m14_SARSTCLMwImmune immune blocks cell infections
+#' @return VK nlmixr function string
+#' @export
+m14_SARSTCLMwImmune = function(fix_E0 = T, ...){
+  m = "function() {
+  ini({
+    tbeta = runif(1,-10,-2)
+    tro = runif(1,-6, -0)
+    tc = runif(1,-20, -2)
+    td = runif(1,-4,3)
+    tk = runif(1,-4,3)
+    tvphi = runif(1,-4,3)
+    tvtheta = runif(1,-4,3)
+    tdImm = runif(1,-4,1)
+    tvirus = runif(1,-2, 2)
+    tinfected_eclipse = runif(1,0, 6)
+    tinfected_lytic = runif(1,0, 6)
+    ttarget = runif(1,0, 6)
+    tImmuneEffector = runif(1,0,6)
+
+    eta.ro ~ 2
+    eta.d ~ 2
+    eta.beta ~ 2
+    eta.k ~ 1
+    eta.vphi ~ 1
+    eta.vtheta ~ 1
+    eta.dImm ~ 1
+    eta.virus ~ 1
+    eta.infected_eclipse ~ 1
+    eta.infected_lytic ~ 1
+    eta.target ~ 1
+    eta.c ~ 2
+    eta.ImmuneEffector ~ 1
+
+    add.sd = 10
+  })
+  model({
+    beta = exp(tbeta + eta.beta)
+    ro = exp(tro + eta.ro)
+    c = exp(tc + eta.c)
+    d = exp(td + eta.d)
+    k = exp(tk + eta.k)
+    vphi = exp(tvphi + eta.vphi)
+    vtheta = exp(tvtheta + eta.vtheta)
+    dImm = exp(tdImm + eta.dImm)
+    
+    virus(0) = exp(tvirus + eta.virus)
+    infected_eclipse(0) = exp(tinfected_eclipse + eta.infected_eclipse)
+    infected_lytic(0) = exp(tinfected_lytic + eta.infected_lytic)
+    target(0) = exp(ttarget + eta.target)
+    ImmuneEffector(0) = exp(tImmuneEffector + eta.ImmuneEffector)
+
+    d/dt(target) = - beta * (1 - ( vphi * (ImmuneEffector / (ImmuneEffector + vtheta) ) ) ) * target * virus
+    d/dt(infected_eclipse) = beta * (1 - ( vphi * (ImmuneEffector / (ImmuneEffector + vtheta) ) ) ) - k * infected_eclipse
+    d/dt(infected_lytic) = k * infected_eclipse - d * infected_lytic
+    d/dt(virus) = ro * infected_lytic - c * virus - beta * virus * target
+    d/dt(ImmuneEffector) = infected_lytic - dImm * ImmuneEffector
+    
+    VL = log(virus)
+    VL ~ add(add.sd)
+  })
+}"
+  
+  # if(fix_E0){
+  #   m = gsub("tE0 = runif(1,-6,1)","",m,fixed = T)
+  #   m = gsub("eta.E0 ~ 2","",m,fixed = T)
+  #   m = gsub("E0 = exp(tE0 + eta.E0)","E0 = 0",m, fixed = T)
+  # }
+  return(m)
+}
+
+#' m15_SARSTCLMwImmune immune increases viral clearance
+#' @return VK nlmixr function string
+#' @export
+m15_SARSTCLMwImmune = function(fix_E0 = T, ...){
+  m = "function() {
+  ini({
+    tbeta = runif(1,-10,-2)
+    tro = runif(1,-6, -0)
+    tc = runif(1,-20, -2)
+    td = runif(1,-4,3)
+    tk = runif(1,-4,3)
+    tvphi = runif(1,-4,3)
+    tvtheta = runif(1,-4,3)
+    tdImm = runif(1,-4,1)
+    tvirus = runif(1,-2, 2)
+    tinfected_eclipse = runif(1,0, 6)
+    tinfected_lytic = runif(1,0, 6)
+    ttarget = runif(1,0, 6)
+    tImmuneEffector = runif(1,0,6)
+
+    eta.ro ~ 2
+    eta.d ~ 2
+    eta.beta ~ 2
+    eta.k ~ 1
+    eta.vphi ~ 1
+    eta.vtheta ~ 1
+    eta.dImm ~ 1
+    eta.virus ~ 1
+    eta.infected_eclipse ~ 1
+    eta.infected_lytic ~ 1
+    eta.target ~ 1
+    eta.c ~ 2
+    eta.ImmuneEffector ~ 1
+
+    add.sd = 10
+  })
+  model({
+    beta = exp(tbeta + eta.beta)
+    ro = exp(tro + eta.ro)
+    c = exp(tc + eta.c)
+    d = exp(td + eta.d)
+    k = exp(tk + eta.k)
+    vphi = exp(tvphi + eta.vphi)
+    vtheta = exp(tvtheta + eta.vtheta)
+    dImm = exp(tdImm + eta.dImm)
+    
+    virus(0) = exp(tvirus + eta.virus)
+    infected_eclipse(0) = exp(tinfected_eclipse + eta.infected_eclipse)
+    infected_lytic(0) = exp(tinfected_lytic + eta.infected_lytic)
+    target(0) = exp(ttarget + eta.target)
+    ImmuneEffector(0) = exp(tImmuneEffector + eta.ImmuneEffector)
+
+    d/dt(target) = - beta * target * virus
+    d/dt(infected_eclipse) = beta * target * virus - k * infected_eclipse
+    d/dt(infected_lytic) = k * infected_eclipse - d * infected_lytic
+    d/dt(virus) = ro * infected_lytic - c * virus - beta * virus * target    - vphi * (ImmuneEffector / (ImmuneEffector + vtheta) ) * virus
+    d/dt(ImmuneEffector) = infected_lytic - dImm * ImmuneEffector
+    
+    VL = log(virus)
+    VL ~ add(add.sd)
+  })
+}"
+  
+  # if(fix_E0){
+  #   m = gsub("tE0 = runif(1,-6,1)","",m,fixed = T)
+  #   m = gsub("eta.E0 ~ 2","",m,fixed = T)
+  #   m = gsub("E0 = exp(tE0 + eta.E0)","E0 = 0",m, fixed = T)
+  # }
+  return(m)
+}
+
+#' m16_SARSTCLMwImmune immune promotes cytotoxicity
+#' @return VK nlmixr function string
+#' @export
+m16_SARSTCLMwImmune = function(fix_E0 = T, ...){
+  m = "function() {
+  ini({
+    tbeta = runif(1,-10,-2)
+    tro = runif(1,-6, -0)
+    tc = runif(1,-20, -2)
+    td = runif(1,-4,3)
+    tk = runif(1,-4,3)
+    tvphi = runif(1,-4,3)
+    tvtheta = runif(1,-4,3)
+    tdImm = runif(1,-4,1)
+    tvirus = runif(1,-2, 2)
+    tinfected_eclipse = runif(1,0, 6)
+    tinfected_lytic = runif(1,0, 6)
+    ttarget = runif(1,0, 6)
+    tImmuneEffector = runif(1,0,6)
+
+    eta.ro ~ 2
+    eta.d ~ 2
+    eta.beta ~ 2
+    eta.k ~ 1
+    eta.vphi ~ 1
+    eta.vtheta ~ 1
+    eta.dImm ~ 1
+    eta.virus ~ 1
+    eta.infected_eclipse ~ 1
+    eta.infected_lytic ~ 1
+    eta.target ~ 1
+    eta.c ~ 2
+    eta.ImmuneEffector ~ 1
+
+    add.sd = 10
+  })
+  model({
+    beta = exp(tbeta + eta.beta)
+    ro = exp(tro + eta.ro)
+    c = exp(tc + eta.c)
+    d = exp(td + eta.d)
+    k = exp(tk + eta.k)
+    vphi = exp(tvphi + eta.vphi)
+    vtheta = exp(tvtheta + eta.vtheta)
+    dImm = exp(tdImm + eta.dImm)
+    
+    virus(0) = exp(tvirus + eta.virus)
+    infected_eclipse(0) = exp(tinfected_eclipse + eta.infected_eclipse)
+    infected_lytic(0) = exp(tinfected_lytic + eta.infected_lytic)
+    target(0) = exp(ttarget + eta.target)
+    ImmuneEffector(0) = exp(tImmuneEffector + eta.ImmuneEffector)
+
+    d/dt(target) = - beta * target * virus
+    d/dt(infected_eclipse) = beta * target * virus - k * infected_eclipse
+    d/dt(infected_lytic) = k * infected_eclipse - d * infected_lytic  - vphi * (ImmuneEffector / (ImmuneEffector + vtheta) ) * infected_lytic
+    d/dt(virus) = ro * infected_lytic - c * virus - beta * virus * target   
+    d/dt(ImmuneEffector) = infected_lytic - dImm * ImmuneEffector
+    
+    VL = log(virus)
+    VL ~ add(add.sd)
+  })
+}"
+  
+  # if(fix_E0){
+  #   m = gsub("tE0 = runif(1,-6,1)","",m,fixed = T)
+  #   m = gsub("eta.E0 ~ 2","",m,fixed = T)
+  #   m = gsub("E0 = exp(tE0 + eta.E0)","E0 = 0",m, fixed = T)
+  # }
+  return(m)
+}
+
+
